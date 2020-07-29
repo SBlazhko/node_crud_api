aa7a9449e287b29c2decd4249d9aee57ff50d691
SBlazhko <serghiy@dvelp.co.uk>
2020-07-29 21:59:22 +0300

    db fix

--- util/database.js
+++ util/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USERNAME}`, `${process.env.DB_PASSWORD}`, {
-    host: `${process.env.DB_HOST}`,
-    dialect: 'postgres'
-});
+// const sequelize = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USERNAME}`, `${process.env.DB_PASSWORD}`, {
+//     host: `${process.env.DB_HOST}`,
+//     dialect: 'postgres'
+// });
+const sequelize = new Sequelize('postgres://blazhkos:1617@10.15.80.3/node-crud-app')
 
 module.exports = sequelize;
